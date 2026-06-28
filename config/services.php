2d37fb101029389bebfb2d6ffb3d8b8d58922b2f
mailduracabs-cpu <mail.duracabs@gmail.com>
2026-06-28 14:22:26 +0530

    Update services.php to include SAMB SMS config
    Removed commented section for third party services and added SAMB SMS configuration.

--- config/services.php
+++ config/services.php
@@ -2,18 +2,6 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
@@ -38,8 +26,15 @@ return [
     'google' => [
         'client_id' => '1094737562052-763eluntljjms0s8nb0taf5735me5o56.apps.googleusercontent.com',
         'client_secret' => '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',
-        //'redirect' => 'http://localhost:8000/auth/google/callback',
         'redirect' => 'https://www.duracabs.com/auth/google/callback',
+    ],
+
+    'sambsms' => [
+        'api_key'     => env('SAMB_SMS_API_KEY'),
+        'entity_id'   => env('SAMB_SMS_ENTITY_ID'),
+        'route_id'    => env('SAMB_SMS_ROUTE_ID'),
+        'sender_id'   => env('SAMB_SMS_SENDER_ID'),
+        'template_id' => env('SAMB_SMS_TEMPLATE_ID'),
     ],
 
 ];
