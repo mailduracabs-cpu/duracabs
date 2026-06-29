9ca5b72274fca92010e6ba8b80450bca50163904
mailduracabs-cpu <mail.duracabs@gmail.com>
2026-06-29 11:35:53 +0530

    Fix class name and update relationships in Price model

--- app/Models/Price.php
+++ app/Models/Price.php
@@ -5,19 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class price extends Model
+class Price extends Model
 {
     use HasFactory;
+
+    protected $table = 'prices';
 
     protected $fillable = [
         'product_id',
         'category_id',
         'price',
-        'max_price'
+        'max_price',
     ];
 
-    public function category(){
-        return $this->belongsTo(Category::class);
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
     }
 
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
 }
