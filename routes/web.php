ad773adeeb4d56a3d3d31d16cb1ae0697228500c
mailduracabs-cpu <mail.duracabs@gmail.com>
2026-06-30 15:09:40 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use App\Livewire\CaroselCard;
 use App\Livewire\CartPage;
 use App\Livewire\CheckoutPage;
 use App\Livewire\ContactUs;
+use App\Livewire\Counter;
+use App\Livewire\EditQueryPage;
 use App\Livewire\Homepage;
 use App\Livewire\MyAccount;
 use App\Livewire\MyOrderDetailPage;
@@ -24,92 +26,61 @@ use App\Livewire\ProductDetailedPage;
 use App\Livewire\RazorePay;
 use App\Livewire\RidesPage;
 use App\Livewire\SuccessPage;
-use App\Livewire\VendorRegistration;
 use App\Livewire\TermsAndConditions;
-use App\Livewire\EditQueryPage;
+use App\Livewire\VendorRegistration;
 use Illuminate\Support\Facades\Route;
 
-use App\Livewire\Counter;
- 
-//public
+Route::get('/', Homepage::class)->name('homepage');
 
-Route::get('optimize-clear', function(){
-	Artisan::call('optimize:clear');
-	return back();
-});
-
-Route::get('/foo', function () {
-    Artisan::call('storage:link');
-});
-
-Route::get('/link', function () {        
-    $target = '/home/fav1u6gtugzi/public_html/duracabs/storage/app/public';
-   $shortcut = '/home/fav1u6gtugzi/public_html/';
-   symlink($target, $shortcut);
-});
-
-Route::get("/storage-link", function () {
-    $targetFolder = storage_path("app/public");
-    $linkFolder = $_SERVER['DOCUMENT_ROOT'] . '/storage';
-    dd($targetFolder, $linkFolder);
-});
-
-
-Route::get('auth/google', [SocialController::class, 'redirectToGoogle']);
-Route::get('auth/google/callback', [SocialController::class, 'handleGoogleCallback']);
-Route::get('/counter', Counter::class);
-Route::get('/razorpay', RazorePay::class)->name('razorpay');
-Route::get('/', Homepage::class);
-Route::get('/cab-categories', CabCategoriesPage::class);
+Route::get('/cab-categories', CabCategoriesPage::class)->name('cab-categories');
 Route::get('/rides', RidesPage::class)->name('rides');
+Route::get('/routes', RidesPage::class)->name('routes');
 Route::get('/edit-query', EditQueryPage::class)->name('edit-query');
-Route::get('/routes', action: RidesPage::class)->name('routes');
 Route::get('/about-us', AboutUs::class)->name('about');
 Route::get('/my-account', MyAccount::class)->name('my-account');
 Route::get('/terms-and-conditions', TermsAndConditions::class)->name('terms-and-conditions');
 Route::get('/cart', CartPage::class)->name('cart');
-Route::get('/route/{slug}', ProductDetailedPage::class);
-Route::get('/pages/{slug}', Page::class);
-Route::get('/page/{slug}', Page::class);
 
-Route::get('reviews', Reviews::class );
+Route::get('/route/{slug}', ProductDetailedPage::class)->name('route.show');
+Route::get('/pages/{slug}', Page::class)->name('pages.show');
+Route::get('/page/{slug}', Page::class)->name('page.show');
 
-Route::get('/card', CaroselCard::class);
+Route::get('/reviews', Reviews::class)->name('reviews');
+Route::get('/card', CaroselCard::class)->name('card');
+Route::get('/counter', Counter::class)->name('counter');
 
-// guest
+Route::get('/vendor-register', VendorRegistration::class)->name('vendor-register');
+Route::get('/contact-us', ContactUs::class)->name('contact-us');
+
+Route::get('/razorpay', RazorePay::class)->name('razorpay');
+
+Route::get('/auth/google', [SocialController::class, 'redirectToGoogle'])->name('auth.google');
+Route::get('/auth/google/callback', [SocialController::class, 'handleGoogleCallback'])->name('auth.google.callback');
+
 Route::middleware('guest')->group(function () {
     Route::get('/login', Login::class)->name('login');
-    Route::get('/register', Register::class);
-   
+    Route::get('/register', Register::class)->name('register');
     Route::get('/forgot', ForgotPassword::class)->name('password.request');
     Route::get('/reset/{token}', ResetPassword::class)->name('password.reset');
 });
 
-Route::get('/vendor-register', VendorRegistration::class);
-Route::get('/contact-us', ContactUs::class);
-
-
-// auth
 Route::middleware('auth')->group(function () {
-    Route::get('/logout', function(){
+    Route::get('/logout', function () {
         auth()->logout();
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+
         return redirect('/');
-    });
+    })->name('logout');
+
     Route::get('/checkout', CheckoutPage::class)->name('checkout');
-    Route::get('/my-orders', MyOrdersPage::class);
+    Route::get('/my-orders', MyOrdersPage::class)->name('my-orders');
     Route::get('/my-orders/{order_id}', MyOrderDetailPage::class)->name('my-orders.show');
     Route::get('/success', SuccessPage::class)->name('success');
     Route::get('/cancel', CancelPage::class)->name('cancel');
-
 });
-Route::get('sitemap.xml', [SiteMapController::class, 'index']);
 
+Route::get('/sitemap.xml', [SiteMapController::class, 'index'])->name('sitemap');
 
-Route::get('razorpay-payment', [RazorpayPaymentController::class, 'index']);
-Route::post('razorpay-payment', [RazorpayPaymentController::class, 'store'])->name('razorpay.payment.store');
-
-
-
-
-
- 
+Route::get('/razorpay-payment', [RazorpayPaymentController::class, 'index'])->name('razorpay.payment.index');
+Route::post('/razorpay-payment', [RazorpayPaymentController::class, 'store'])->name('razorpay.payment.store');
