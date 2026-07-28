6c497d48e46ca9d192f2e3cd705cb8c284257de1
mailduracabs-cpu <mail.duracabs@gmail.com>
2026-07-28 12:31:08 +0530

    Fix homepage JS crash caused by Preline autoInit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import './bootstrap';
-import 'preline';
+
+// Temporarily disabled because Preline autoInit is crashing the homepage.
+// import 'preline';
+
 import './modules/homepage';
 import './modules/rides';
 import './modules/cookie-consent';
